a4fb1e901a0a9300f4e1c964d737fd8c241170db
itsechi <wardulinskao@gmail.com>
2022-08-26 15:57:03 +0200

    Add a test Todo class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './sass/main.scss';
+import Todo from './modules/createTodo';
+
+const task1 = new Todo('Learn JavaScript', 'Finish the course and start working on the project', '10/09/22', 'high');
+console.log(task1);
