fb52effccc7e2e172885ed9c51abd3a50de1844d
itsechi <wardulinskao@gmail.com>
2022-08-27 14:57:35 +0200

    Add functions to close form

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,11 +1,33 @@
+import Todo from "./createTodo";
+
 export default function UI() {
   const openTodoFormBtn = document.getElementById('openTodoFormBtn');
+  const closeTodoFormBtn = document.getElementById('closeTodoFormBtn');
   const overlay = document.getElementById('overlay');
   const todoForm = document.getElementById('todoForm');
-  openTodoFormBtn.addEventListener('click', displayTodoForm);
+  const addTodoBtn = document.getElementById('addTodoBtn');
 
+  // handlers
+  openTodoFormBtn.addEventListener('click', displayTodoForm);
+  overlay.addEventListener('click', closeTodoForm);
+  closeTodoFormBtn.addEventListener('click', closeTodoForm);
+  addTodoBtn.addEventListener('click', addTodo);
+
+  // functions
   function displayTodoForm() {
     overlay.classList.remove('hidden');
     todoForm.classList.remove('hidden');
+  }
+
+  function closeTodoForm() {
+    overlay.classList.add('hidden');
+    todoForm.classList.add('hidden');
+  }
+
+  function addTodo(e) {
+    e.preventDefault();
+    closeTodoForm();
+    const todo = new Todo();
+    console.log(todo);  
   }
 }
