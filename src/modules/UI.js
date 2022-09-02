374dbd07e1093cef6fa0d5b7a3a43fbd7ca888e1
itsechi <wardulinskao@gmail.com>
2022-09-02 18:54:08 +0200

    Modify the createTodo form and add a function to edit todos

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -5,26 +5,54 @@ import { format } from 'date-fns';
 const UI = (() => {
   const container = document.getElementById('todoContainer');
   const todos = createTodo.unfinishedTodos;
+  const overlay = document.getElementById('overlay');
+  const todoForm = document.getElementById('todoForm');
 
   function createTodoForm() {
     const openTodoFormBtn = document.getElementById('openTodoFormBtn');
-    const closeTodoFormBtn = document.getElementById('closeTodoFormBtn');
-    const overlay = document.getElementById('overlay');
-    const todoForm = document.getElementById('todoForm');
-    const addTodoBtn = document.getElementById('addTodoBtn');
-    const dueDateBtn = document.getElementById('dueDateBtn');
-
-    // handlers
     openTodoFormBtn.addEventListener('click', displayTodoForm);
-    overlay.addEventListener('click', closeTodoForm);
-    closeTodoFormBtn.addEventListener('click', closeTodoForm);
-    addTodoBtn.addEventListener('click', addTodo);
 
     // functions
     function displayTodoForm() {
       overlay.classList.remove('hidden');
       todoForm.classList.remove('hidden');
+
+      const html = `
+      <div class="form__header">
+        <h2 class="form__title">NEW TASK</h2>
+        <span class="icon icon--bold material-icons-outlined" id="closeTodoFormBtn">close</span>
+      </div>
+
+      <form class="form__form">
+        <div class="form__inputs">
+          <input class="form__text form__text--bold" type="text" placeholder="Title of the task" id="todoTitle">
+          <textarea class="form__text" rows="3" cols="0" placeholder="Description" id="todoDescription"></textarea>
+          <div class="form__buttons">
+            <input class="btn btn--secondary" type="date" id="dueDateBtn"></button>
+            <select class="btn btn--secondary" id="projectBtn">
+              <option disabled selected value>PROJECT</option>
+            </select>
+            <select class="btn btn--secondary" id="priorityBtn">
+              <option disabled selected value>PRIORITY</option>
+              <option value="high">HIGH</option>
+              <option value="medium">MEDIUM</option>
+              <option value="low">LOW</option>
+            </select>
+          </div>
+        </div>
+        <button class="btn btn--primary" id="addTodoBtn">ADD TASK</button>
+      </form>`;
+      todoForm.innerHTML = html;
+
+      const addTodoBtn = document.getElementById('addTodoBtn');
+      const closeTodoFormBtn = document.getElementById('closeTodoFormBtn');
+      const dueDateBtn = document.getElementById('dueDateBtn');
       dueDateBtn.valueAsDate = new Date();
+
+      // handlers
+      overlay.addEventListener('click', closeTodoForm);
+      closeTodoFormBtn.addEventListener('click', closeTodoForm);
+      addTodoBtn.addEventListener('click', addTodo);
     }
 
     function closeTodoForm() {
@@ -35,6 +63,7 @@ const UI = (() => {
       document.getElementById('projectBtn').value = '';
       document.getElementById('priorityBtn').value = '';
       dueDateBtn.valueAsDate = new Date();
+      todoForm.innerHTML = '';
     }
 
     function addTodo(e) {
@@ -47,55 +76,25 @@ const UI = (() => {
 
   function displayTodos() {
     container.innerHTML = '';
-    // delete later
-    // const task1 = new Todo(
-    //   'Learn JavaScript',
-    //   'Finish the course and start working on the project',
-    //   '2022-08-29',
-    //   '',
-    //   'high'
-    // );
-    // const task2 = new Todo(
-    //   'Learn JavaScript',
-    //   'Finish the course and start working on the project',
-    //   '2022-08-28',
-    //   '',
-    //   'medium'
-    // );
-    // const task3 = new Todo(
-    //   'Learn JavaScript',
-    //   'Finish the course and start working on the project',
-    //   '2022-11-05',
-    //   '',
-    //   'low'
-    // );
-    // todos.push(task1, task2, task3);
-    //
     todos.forEach((todo, index) => {
       const today = format(new Date(), 'yyyy-MM-dd');
       const date = format(new Date(todo.dueDate), 'dd MMM yyyy');
-      let html = `<div class="todo" data-index=${index}>
-    <div class="todo__left">
-      <input class="todo__check" type="checkbox" name="" id="">
-      <div class="todo__text">
-        <h3 class="todo__title">${
-          todo.title
-        }<span class="todo__priority todo__priority--${
-        todo.priority
-      }"></span></h3>
-      <p class="todo__description hidden">${todo.description}</p>
-        <p class="todo__date">${today === todo.dueDate ? 'Today' : date}</p>
-      </div>
-    </div>
-    <div class="todo__right">
-    <span class="icon material-icons-outlined">
-    edit
-    </span>
-<span class="delete icon material-icons-outlined">
-delete
-</span>
-    </div>
-  </div>`;
+      // prettier-ignore
+      const html = `
+      <div class="todo" data-index=${index}>
+        <div class="todo__left">
+          <input class="todo__check" type="checkbox" name="" id="">
+          <div class="todo__text">
+            <h3 class="todo__title">${todo.title}<span class="todo__priority todo__priority--${todo.priority}"></span></h3>
+            <p class="todo__description hidden">${todo.description}</p>
+            <p class="todo__date">${today === todo.dueDate ? 'Today' : date}</p>
+          </div>
+        </div>
+        <div class="todo__right">
+          <span class="edit icon material-icons-outlined">edit</span>
+          <span class="delete icon material-icons-outlined">delete</span>
+        </div>
+      </div>`;
       container.insertAdjacentHTML('beforeend', html);
     });
   }
@@ -127,13 +126,85 @@ delete
     container.addEventListener('click', e => {
       if (e.target.classList.contains('delete')) {
         const todoIndex = e.target.closest('.todo').dataset.index;
-        const currTodo = todos[todoIndex];
-        console.log(todos[todoIndex]);
-        console.log(todoIndex);
-        console.log(currTodo);
         todos.splice(todoIndex, 1);
-        console.log(todos);
         displayTodos();
+      }
+    });
+  }
+
+  function createEditForm() {
+    container.addEventListener('click', e => {
+      if (e.target.classList.contains('edit')) {
+        const todoIndex = e.target.closest('.todo').dataset.index;
+        const currTodo = todos[todoIndex];
+        displayEditForm();
+
+        const title = document.getElementById('todoTitle');
+        const description = document.getElementById('todoDescription');
+        const dueDate = document.getElementById('dueDateBtn');
+        const project = document.getElementById('projectBtn');
+        const priority = document.getElementById('priorityBtn');
+        title.value = currTodo.title;
+        description.value = currTodo.description;
+        dueDate.value = currTodo.dueDate;
+        project.value = currTodo.project;
+        priority.value = currTodo.priority;
+
+        function displayEditForm() {
+          overlay.classList.remove('hidden');
+          todoForm.classList.remove('hidden');
+
+          const html = `
+          <div class="form__header">
+            <h2 class="form__title">EDIT TASK</h2>
+            <span class="icon icon--bold material-icons-outlined" id="closeEditFormBtn">close</span>
+          </div>
+
+          <form class="form__form">
+            <div class="form__inputs">
+              <input class="form__text form__text--bold" type="text" placeholder="Title of the task" id="todoTitle">
+              <textarea class="form__text" rows="3" cols="0" placeholder="Description" id="todoDescription" ></textarea>
+              <div class="form__buttons">
+                <input class="btn btn--secondary" type="date" id="dueDateBtn"></button>
+                <select class="btn btn--secondary" id="projectBtn">
+                  <option disabled selected value>PROJECT</option>
+                </select>
+                <select class="btn btn--secondary" id="priorityBtn">
+                  <option disabled selected value>PRIORITY</option>
+                  <option value="high">HIGH</option>
+                  <option value="medium">MEDIUM</option>
+                  <option value="low">LOW</option>
+                </select>
+              </div>
+            </div>
+            <button class="btn btn--primary" id="editTodoBtn">EDIT TASK</button>
+          </form>`;
+
+          todoForm.innerHTML = html;
+          const editTodoBtn = document.querySelector('#editTodoBtn');
+          const closeEditBtn = document.querySelector('#closeEditFormBtn');
+
+          // handlers
+          closeEditBtn.addEventListener('click', closeEditForm);
+          editTodoBtn.addEventListener('click', pushEdit);
+        }
+
+        function closeEditForm() {
+          overlay.classList.add('hidden');
+          todoForm.classList.add('hidden');
+          todoForm.innerHTML = '';
+        }
+
+        function pushEdit(e) {
+          e.preventDefault();
+          currTodo.title = title.value;
+          currTodo.description = description.value;
+          currTodo.dueDate = dueDate.value;
+          currTodo.project = project.value;
+          currTodo.priority = priority.value;
+          closeEditForm();
+          displayTodos();
+        }
       }
     });
   }
@@ -144,6 +215,7 @@ delete
     displayDetails();
     markComplete();
     removeTodo();
+    createEditForm();
   }
 
   return { initialize };
--- src/modules/createTodo.js
+++ src/modules/createTodo.js
@@ -4,11 +4,11 @@ export const createTodo = (() => {
   const unfinishedTodos = [];
 
   function addTodo() {
-    let title = document.getElementById('todoTitle').value;
-    let description = document.getElementById('todoDescription').value;
-    let dueDate = document.getElementById('dueDateBtn').value;
-    let project = document.getElementById('projectBtn').value;
-    let priority = document.getElementById('priorityBtn').value;
+    const title = document.getElementById('todoTitle').value;
+    const description = document.getElementById('todoDescription').value;
+    const dueDate = document.getElementById('dueDateBtn').value;
+    const project = document.getElementById('projectBtn').value;
+    const priority = document.getElementById('priorityBtn').value;
 
     if (!title || title.trim().length === 0) return;
     const todo = new Todo(title, description, dueDate, project, priority);
